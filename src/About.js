af76d3f9bd40a2a1e31c9ab62367d713c741d8bf
Hamza Younis <hamzayounis106@gmail.com>
2024-02-14 05:43:12 +0500

    About page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Lottie from "lottie-react";
+import LottieRefCurrentProps from "lottie-react";
+import animationData from "./Animation/AboutPageAnimation.json";
+import { useRef } from "react";
+import "./style.css"
 
 function About() {
+  const cookRef = useRef < LottieRefCurrentProps > null;
   return (
-    <div>
-      ABout
-    </div>
-  )
+    <>
+      <div className="about_div">
+        <div className="animation_div">
+          <Lottie
+            lottieRef={cookRef}
+            animationData={animationData}
+            width={40}
+          />
+        </div>
+        <div className="about_text_div">
+          <h2>About Us</h2>
+          <p>Welcome to our culinary haven, where every dish is a narrative woven with passion and creativity. At our kitchen, we're dedicated to crafting not just meals, but experiences that tantalize the senses and spark joy in every bite. Our journey began with a shared love for food and a desire to explore its infinite possibilities. From humble beginnings to culinary adventures, we've curated a collection of recipes that reflect our commitment to innovation and excellence. Join us as we share the stories behind each creation, inviting you to embark on a flavorful voyage that celebrates the artistry of cooking. Whether you're a seasoned chef or an aspiring home cook, there's something here for everyone to savor and enjoy. Indulge your curiosity, awaken your taste buds, and let our recipes inspire your culinary exploration.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
